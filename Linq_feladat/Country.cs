88f6c83ee5ae52f13a19c05fe8676a7294643d4d
Ferenc Hegedüs <ferenc.hegedus@stud.uni-corvinus.hu>
2022-11-18 20:06:24 +0100

    feladat 1

---
+++ Linq_feladat/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_feladat
+{
+    class Country
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
