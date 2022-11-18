cb891e0c1ec7b2fdf08fcbfc05e340940298c81c
Ferenc Hegedüs <ferenc.hegedus@stud.uni-corvinus.hu>
2022-11-18 20:28:26 +0100

    feladat 4

--- Linq_feladat/Form1.cs
+++ Linq_feladat/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,73 @@ namespace Linq_feladat
 {
     public partial class Form1 : Form
     {
+        List<Country> countries = new List<Country>();
+        List<Ramen> ramens = new List<Ramen>();
+        List<Brand> brands = new List<Brand>();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData("ramen.csv");
+
+        }
+
+        void LoadData(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            sr.ReadLine(); //átugrik az első soron
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                string orszag = sor[2];
+                string marka = sor[0];
+                //    var ered = countries.Where(i => i.Name.Equals(orszag)).FirstOrDefault(); //LINQ
+                Country aktorszag = AddCountry(orszag); //aktuális ország
+                Brand aktmarka = AddBrand(marka);
+                Ramen r = new Ramen
+                {
+                    ID = ramens.Count,
+                    CountryFK = aktorszag.ID,
+                    Country = aktorszag,
+                    Rating = Convert.ToDouble(sor[3]),
+                    Name = aktorszag.Name,
+                    Brand = aktmarka
+                };
+                ramens.Add(r);
+            }
+            sr.Close();
+
+
+        }
+
+        Country AddCountry(string orszag)
+        {
+            var ered = (from c in countries where c.Name.Equals(orszag) select c).FirstOrDefault();
+            if (ered == null)  //nincs ilyen ország a listában
+            {
+                ered = new Country
+                {
+                    ID = countries.Count,
+                    Name = orszag
+                };
+                countries.Add(ered);
+            }
+            return ered;
+        }
+
+        Brand AddBrand(string marka)
+        {
+            var ered = (from c in brands where c.Name.Equals(marka) select c).FirstOrDefault();
+            if (ered == null)  //nincs ilyen ország a listában
+            {
+                ered = new Brand
+                {
+                    ID = brands.Count,
+                    Name = marka
+                };
+                brands.Add(ered);
+            }
+            return ered;
         }
     }
 }
