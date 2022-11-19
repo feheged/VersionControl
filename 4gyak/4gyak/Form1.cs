84595e0f5e5bc68d3ba5e44e1e6f623b1c8c8284
Ferenc Hegedüs <ferenc.hegedus@stud.uni-corvinus.hu>
2022-11-19 17:46:12 +0100

    feladat07

--- 4gyak/4gyak/Form1.cs
+++ 4gyak/4gyak/Form1.cs
@@ -97,6 +97,25 @@ namespace _4gyak
 
         }
 
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
