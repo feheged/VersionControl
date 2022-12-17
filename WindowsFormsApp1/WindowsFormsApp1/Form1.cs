74ac6d05f22c495efe1f449356ef4ba71cdf1f0e
Ferenc Hegedüs <ferenc.hegedus@stud.uni-corvinus.hu>
2022-12-17 13:59:57 +0100

    feladat7

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -41,5 +41,22 @@ namespace WindowsFormsApp1
 
 
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
+
     }
 }
