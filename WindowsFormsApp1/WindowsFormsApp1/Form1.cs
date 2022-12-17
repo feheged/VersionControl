902fe8a14bb4714d75cccc5eadb523db2c286f15
Ferenc Hegedüs <ferenc.hegedus@stud.uni-corvinus.hu>
2022-12-17 13:43:36 +0100

    feladat4.5

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
         }
     }
 }
